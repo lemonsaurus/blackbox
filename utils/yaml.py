b7fdd14fb0048d98423618888c3c74434976ae32
Leon Sandøy <leon.haland@gmail.com>
2020-12-29 19:54:03 +0100

    Raise an exception if no config.yaml exists.

--- utils/yaml.py
+++ utils/yaml.py
@@ -2,9 +2,18 @@ import pathlib
 
 import yaml
 
+from config.exceptions import ImproperlyConfigured
+
 
 def get_yaml_config() -> dict:
     """Load the yaml file in the root folder, and return it as a dict."""
     root_folder = pathlib.Path(__file__).parent.parent.absolute()
-    with open(root_folder / "config.yaml", encoding="UTF-8") as f:
-        return yaml.safe_load(f)
+
+    try:
+        with open(root_folder / "config.yaml", encoding="UTF-8", mode="r") as f:
+            return yaml.safe_load(f)
+    except FileNotFoundError as e:
+        raise ImproperlyConfigured(
+            "You must create a config.yaml file in the root folder! "
+            "See the readme under 'Configuration' for more information."
+        ) from e
