98ae001038f461d6f697376332136412c3d31778
Leon Sandøy <leon.sandoy@conmodo.com>
2021-01-01 11:57:22 +0100

    Change BlackBoxDatabase abstract class spec.
    All BlackBoxDatabase subclasses must now implement two new methods,
    _get_connstring and _parse_connstring. These methods will be used to
    fetch and parse the connstring from the config so that we can figure out
    what to connect to.

--- databases/_base.py
+++ databases/_base.py
@@ -3,6 +3,29 @@ from pathlib import Path
 
 
 class BlackBoxDatabase(ABC):
+    """An abstract database handler."""
+
+    @abstractmethod
+    def _get_connstring(self):
+        """Try to get the connstring out of the config."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def _parse_connstring(self):
+        """Parse the connstring and return its constituent parts."""
+        raise NotImplementedError
+
+    @property
+    def enabled(self):
+        """
+        A property that tells us whether the handler is enabled or not.
+
+        This only has to be overridden if you need some sort of custom logic for it.
+        """
+        if self._get_connstring():
+            return True
+        return False
+
     @abstractmethod
     def backup(self) -> Path:
         """
