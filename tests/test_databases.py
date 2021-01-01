951bc37bb18989b6226954bd3eaa58967e84d39e
Leon Sandøy <leon.sandoy@conmodo.com>
2021-01-01 11:57:53 +0100

    Update database tests for new connstring methods.

--- tests/test_databases.py
+++ tests/test_databases.py
@@ -1,5 +1,6 @@
 import unittest
 
+from config import BlackBox
 from config.exceptions import ImproperlyConfigured
 from databases import MongoDB, Postgres, Redis
 
@@ -13,12 +14,24 @@ class BlackBoxDatabaseTests(unittest.TestCase):
         except ImproperlyConfigured:
             self.fail("Mongo() could not be instantiated.")
 
-    def test_postgres_handler_can_be_instantiated(self):
+    def test_postgres_handler_can_be_instantiated_with_one_connstring(self):
         """Test if the PostgreSQL database handler can be instantiated."""
         try:
+            BlackBox.databases = [
+                "postgres://user:password@host:port",
+            ]
             Postgres()
         except ImproperlyConfigured:
             self.fail("Postgres() could not be instantiated.")
+
+    def test_postgres_fails_with_multiple_connstrings(self):
+        """Test if the PostgreSQL database handler can be instantiated with multiple connstrings."""
+        with self.assertRaises(ImproperlyConfigured):
+            BlackBox.databases = [
+                "postgres://johnwasafraid",
+                "postgres://lellowmelon"
+            ]
+            Postgres()
 
     def test_redis_handler_can_be_instantiated(self):
         """Test if the Redis database handler can be instantiated."""
