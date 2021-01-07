137246de7f6949b73bc06d8f212896e82cd03127
Leon Sandøy <leon.sandoy@conmodo.com>
2021-01-07 14:13:57 +0100

    Implement MongoDB handler.
    This will dump a gzipped archive file and return it.

--- databases/mongodb.py
+++ databases/mongodb.py
@@ -1,19 +1,36 @@
+import datetime
 import logging
 from pathlib import Path
 
 from databases._base import BlackboxDatabase
+from utils import run_command
 
 log = logging.getLogger(__name__)
 
 
 class MongoDB(BlackboxDatabase):
-    """A Database handler that will do a mongodump for MongoDB, backing up all documents."""
+    """
+    A Database handler that will do a mongodump for MongoDB, backing up all documents.
 
-    connstring_regex = r"mongo(?:db)?://(?P<user>.+):(?P<password>.+)@(?P<host>.+):(?P<port>.+)"
+    This will use mongodump with --gzip and --archive, and must be restored using the same
+    arguments, e.g. mongorestore --gzip --archive=/path/to/file.archive.
+    """
+
+    connstring_regex = r"mongodb://(?P<user>.+):(?P<password>.+)@(?P<host>.+):(?P<port>.+)"
     valid_uri_protocols = [
-        "mongo",
-        "mongodb",
+        "mongodb"
     ]
 
     def backup(self) -> Path:
-        raise NotImplementedError
+        """Dump all the data to a file and then return the filepath."""
+        date = datetime.date.today().strftime("%d_%m_%Y")
+        archive_file = Path.home() / f"mongodb_blackbox_{date}.archive"
+        output = run_command(
+            f"mongodump "
+            f"--uri={self.connstring} "
+            "--gzip "
+            "--forceTableScan "
+            f"--archive={archive_file}"
+        )
+        log.debug(output)
+        return archive_file
