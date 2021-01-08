e8e29f0cad4e43dc8941d23e3376b887500fc1cd
Leon Sandøy <leon.sandoy@conmodo.com>
2021-01-08 14:38:27 +0100

    Notify all notifiers in main.py.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from blackbox.handlers import all_databases, all_storage_providers, all_notifiers
 
 if __name__ == "__main__":
-    report = {}
+    report = {
+        "output": "",
+        "success": True,
+        "databases": {},
+    }
     for DatabaseHandler in all_databases:
         database = DatabaseHandler()
+        database_type = DatabaseHandler.__name__
 
         if not database.enabled:
             continue
@@ -12,10 +17,14 @@ if __name__ == "__main__":
         backup_file = database.backup()
 
         # Add the outcome to the report.
-        report[DatabaseHandler.__name__] = {
-            "state":  "success" if database.success else "failed",
-            "output": database.output,
+        report['databases'][database_type] = {
+            "type": database_type,
+            "storage": [],
         }
+
+        # If one database failed, the overall report is a failure.
+        report['output'] += database.output if not database.success else ""
+        report['success'] = database.success if not database.success else report['success']
 
         # Check the outcome of the backup.
         # If it failed, we'll add it to the report and continue
@@ -26,10 +35,28 @@ if __name__ == "__main__":
         # Otherwise, sync the backup file to every provider.
         for StorageProvider in all_storage_providers:
             provider = StorageProvider()
+            provider_type = StorageProvider.__name__
 
             if not provider.enabled:
                 continue
 
+            # Sync the provider, and store the outcome to the report.
             provider.sync(backup_file)
+            report['databases'][database_type]['storage'].append({
+                "type": provider_type,
+                "success": provider.success,
+            })
+            report['output'] += provider.output
 
-    print(report)
+            # If one storage handler failed, the overall report is a failure.
+            report['success'] = provider.success if not provider.success else report['success']
+
+    # Now send a report to all notifiers.
+    for Notifier in all_notifiers:
+        notifier = Notifier()
+
+        if not notifier.enabled:
+            continue
+
+        # Send a notification
+        notifier.notify(report)
