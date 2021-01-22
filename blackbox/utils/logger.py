58011c850982e04f97bc71b927137b4ab05bd89f
Leon Sandøy <leon.sandoy@conmodo.com>
2021-01-22 01:14:38 +0100

    Set up logging with loguru.

---
+++ blackbox/utils/logger.py
@@ -0,0 +1,12 @@
+import sys
+
+from loguru import logger
+
+# Remove all pre-configured loggers. We just want the ones we're about to add
+logger.remove(None)
+
+# Add a simple logger with no fluff.
+logger.add(sys.stderr, format="<yellow>{time:HH:mm:ss}</yellow> {message}", colorize=True)
+
+# All other files will import from here.
+log = logger.opt(colors=True)
