744fee529158d1cdf24c14ab59b3d896dd0dd1c8
ks129 <45097959+ks129@users.noreply.github.com>
2021-01-23 10:37:20 +0200

    Add Redis database handler to all database handlers list

--- blackbox/handlers/__init__.py
+++ blackbox/handlers/__init__.py
@@ -1,11 +1,13 @@
 from .databases.mongodb import MongoDB
 from .databases.postgres import Postgres
+from .databases.redis import Redis
 from .notifiers.discord import Discord
 from .storage.s3 import S3
 
 all_databases = [
     MongoDB,
     Postgres,
+    Redis,
 ]
 
 all_storage_providers = [
