0f38ac956310c6c8a8d5f2c397c982eb43d71df4
Christopher Baklid <christopherb@unity3d.com>
2021-01-24 20:17:11 +0100

    remove the exception hack

--- blackbox/exceptions.py
+++ blackbox/exceptions.py
@@ -1,8 +1,2 @@
-import sys
-
-
 class ImproperlyConfigured(Exception):
     """A class for raising configuration errors in our handlers."""
-    def __init__(self, msg):
-        self.args = "ERROR: {0}".format(msg),
-        sys.exit(self)
