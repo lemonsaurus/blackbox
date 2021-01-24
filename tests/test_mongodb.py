fef31884715a833474b254ddee3c29a679d170c6
Christopher Baklid <christopherb@unity3d.com>
2021-01-24 21:53:43 +0100

    lint

--- tests/test_mongodb.py
+++ tests/test_mongodb.py
@@ -1,6 +1,6 @@
-import pytest
 import datetime
 from pathlib import Path
+
 
 def test_mongodb_backup(config_file, mocker, fake_process):
     """Test if the MongoDB database handler executes a backup"""
@@ -13,8 +13,8 @@ def test_mongodb_backup(config_file, mocker, fake_process):
     archive = Path.home() / f"mongodb_blackbox_{date}.archive"
 
     command_to_run = [
-            f"mongodump --uri= --gzip --forceTableScan --archive={archive}"
-        ]
+        f"mongodump --uri= --gzip --forceTableScan --archive={archive}"
+    ]
 
     fake_process.register_subprocess(
         command_to_run, stdout=["thing", "stuff"]
