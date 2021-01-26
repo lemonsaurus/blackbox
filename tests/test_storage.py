643498b7f4dd5177d6fad4085605a1d3f7e25835
Christopher Baklid <christopherb@unity3d.com>
2021-01-26 17:38:25 +0100

    fix s3 config checks

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,11 +1,11 @@
-def test_s3_handler_can_be_instantiated(config_file):
-    """Test if the GoogleDrive storage handler can be instantiated."""
-    pass
-    # from blackbox.handlers.storage import S3
+import pytest
 
-    # This currently fails with
-    #         with open(full_path, 'rb') as fp:
-    # >           payload = fp.read().decode('utf-8')
-    # E           AttributeError: 'str' object has no attribute 'decode'
-    # .venv\lib\site-packages\botocore\loaders.py:173: AttributeError
-    # S3()
+from blackbox.exceptions import ImproperlyConfigured
+from blackbox.handlers.storage import S3
+
+
+def test_s3_handler_can_be_instantiated(config_file):
+    """Test if the s3 storage handler can be instantiated."""
+
+    with pytest.raises(ImproperlyConfigured):
+        S3()
