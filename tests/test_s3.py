821299380a931915d68943dbd7863269f704a843
Christopher Baklid <christopherb@unity3d.com>
2021-02-07 12:46:04 +0100

    wip

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import pytest
 
 from blackbox.exceptions import ImproperlyConfigured
@@ -5,39 +8,45 @@ from blackbox.handlers.storage import S3
 
 
 def test_s3_missing_aws_file(config_file):
-    """Test if the s3 storage handler can be instantiated."""
+    """Test raising ImproperlyConfigured when the aws config file is missing."""
 
-    with pytest.raises(AttributeError):
+    with pytest.raises(ImproperlyConfigured):
         # In this case the "aws_secret_access_key" is missing
         S3()
 
 
 def test_s3_endpoint(config_file, mocker):
-    """Test if the s3 storage handler can be instantiated."""
+    """Test if the s3 storage handler can be instantiated with environment variables."""
 
+    os.environ["aws_access_key_id"] = "some"
+    os.environ["aws_secret_access_key"] = "key"
     S3.config = {
-        "user": "internet",
-        "password": "dingdong",
-        "aws_access_key_id": "somekey",
-        "aws_secret_access_key": "somekey",
+        "username": "john",
+        "password": "mcgee",
+        "s3_endpoint": "s3://s3.endpoint.com/",
+        "bucket_name": "bucket"
     }
 
-    print(S3.config)
-    s3 = S3()
-    print(s3.connstring)
+    data = json.dumps({
+        "partitions": [{
+            "defaults": {
+                "hostname": r"{service}.{region}.{dnsSuffix}",
+                "protocols": ["https"],
+                "signatureVersions": ["v4"],
+                "serviceId": "myservice"
+            },
+            "dnsSuffix": "amazonaws.com",
+            "partition": "aws",
+            "partitionName": "AWS Standard",
+            "regionRegex": r"^(us|eu|ap|sa|ca|me|af)\-\w+\-\d+$",
+            "regions": {
+                "eu-central-1": {
+                    "description": "Europe (Frankfurt)"
+                },
+            }
+        }]
+    })
 
-    exit(1)
+    mocker.patch("builtins.open", mocker.mock_open(read_data=data.encode()))
 
-
-def test_s3_handler_can_be_instantiated(config_file):
-    """Test if the s3 storage handler can be instantiated."""
-
-    with pytest.raises(ImproperlyConfigured):
-        # In this case the "aws_secret_access_key" is missing
-        S3.config = {
-            "user": "internet",
-            "password": "dingdong",
-            "aws_access_key_id": "somekey",
-        }
-
-        S3()
+    S3()
