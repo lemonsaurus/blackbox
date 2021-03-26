26f6754f9f4bdbd6c06599cf5638496dab1815a6
kosayoda <kieransiek@protonmail.com>
2021-03-26 21:54:18 +0800

    Improve s3 tests.
    Add tests for getting the aws credentials from the environment variables
    or .aws credential files.

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 
 from blackbox.exceptions import MissingFields
@@ -5,21 +7,49 @@ from blackbox.handlers.storage import S3
 
 
 @pytest.fixture
-def mock_valid_s3_config():
+def mock_valid_s3_config_without_aws_credentials():
     return {"bucket": "bigbucket", "endpoint": "s3.endpoint.com"}
 
 
-@pytest.fixture
-def mock_invalid_s3_config():
-    return {"bucket": "smolbucket"}
-
-
-def test_s3_handler_can_be_instantiated_with_required_fields(mock_valid_s3_config):
+def test_s3_handler_can_be_instantiated_with_required_fields():
     """Test if the s3 storage handler can be instantiated."""
-    S3(**mock_valid_s3_config)
+    valid_config = {
+        "bucket": "bigbucket", "endpoint": "s3.endpoint.com",
+        "aws_access_key_id": "lemon", "aws_secret_access_key": "dance"
+    }
+    S3(**valid_config)
 
 
-def test_s3_handler_fails_without_required_fields(mock_invalid_s3_config):
+def test_s3_handler_can_be_instantiated_with_env_variables(
+        monkeypatch, mock_valid_s3_config_without_aws_credentials
+):
+    """Test if the s3 storage handler can be instantiated with key & secret from env variables."""
+    # Add them to the environment
+    monkeypatch.setenv("AWS_ACCESS_KEY_ID", "lemon")
+    monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "dance")
+
+    S3(**mock_valid_s3_config_without_aws_credentials)
+
+
+def test_s3_handler_can_be_instantiated_with_aws_config_files(
+        monkeypatch, tmp_path, mock_valid_s3_config_without_aws_credentials
+):
+    """Test if the s3 storage handler can be instantiated with key & secret from .aws/ files."""
+    # Make valid config files
+    aws_folder = tmp_path / ".aws"
+    aws_folder.mkdir()
+    aws_folder.joinpath("config").touch()
+    aws_folder.joinpath("credentials").touch()
+
+    # Patch pathlib.Path.home() to the temporary directory
+    with monkeypatch.context() as m:
+        m.setattr(Path, "home", lambda: tmp_path)
+        S3(**mock_valid_s3_config_without_aws_credentials)
+
+
+def test_s3_handler_fails_without_required_fields():
     """Test if the s3 storage handler cannot be instantiated with missing fields."""
+    incomplete_config = {"bucket": "smolbucket"}
+
     with pytest.raises(MissingFields):
-        S3(**mock_invalid_s3_config)
+        S3(**incomplete_config)
