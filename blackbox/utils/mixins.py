fef1b8164839d62545120a5d0144b8aabd9defc1
Denis Matakov <denis@matakov.com>
2021-04-05 20:50:04 +0300

    Upgrade mixin class to use all credentials not only databases

--- blackbox/utils/mixins.py
+++ blackbox/utils/mixins.py
@@ -1,9 +1,38 @@
+from blackbox.config import Blackbox as CONFIG
+from blackbox.utils.logger import log
+
+
 class SanitizeReportMixin:
-    @staticmethod
-    def sanitize_output(config: dict, sensitive_output: str) -> str:
+    """
+    Mixin class helper to mask all sensitive credentials
+
+    Could handle any string with sensitive output
+    """
+
+    def sanitize_output(self, sensitive_output: str) -> str:
         """ Replace all self.config credentials with in any str *** """
-        for sensitive_word in config.values():
+
+        # first of all let's combine list of secrets from config
+        sensitive_words = []
+        mixed_secrets = [CONFIG.databases.values(), CONFIG.storage.values(),
+                         CONFIG.notifiers.values(), ]
+        for mixed_secret in mixed_secrets:
+            self.extract_secrets(sensitive_words, mixed_secret)
+
+        # now it's time to mask all secrets in our output
+        for sensitive_word in set(sensitive_words):
             sensitive_output = sensitive_output.replace(
                 sensitive_word, "*" * len(sensitive_word))
-        sanitized_output = sensitive_output  # emphasize output is clear now
+
+        # emphasize output is clear now
+        sanitized_output = sensitive_output
+        log.success("Secrets are masked!")
         return sanitized_output
+
+    @staticmethod
+    def extract_secrets(sensitive_words: [], mixed_secret) -> str:
+        """ Helper method to get secrets values only """
+        for database_name in mixed_secret:
+            for secrets in database_name.values():
+                sensitive_words += list(secrets.values())
+        return sensitive_words
