56467e04adfc124d00b6c3ef9c902b82b9f3345f
Denis Matakov <denis@matakov.com>
2021-04-07 12:47:38 +0300

    Postgres and Redis are support now integer port values.
    And now port number is not required because we have default ports for popular databases: 5432 for postgres and 6379 for redis

--- blackbox/handlers/databases/postgres.py
+++ blackbox/handlers/databases/postgres.py
@@ -9,7 +9,7 @@ from blackbox.utils.logger import log
 class Postgres(BlackboxDatabase):
     """A Database handler that will do a pg_dumpall for Postgres, backing up all tables."""
 
-    required_fields = ("username", "password", "host", "port")
+    required_fields = ("username", "password", "host", )
 
     def backup(self) -> Path:
         """Dump all the data to a file and then return the filepath."""
@@ -22,7 +22,7 @@ class Postgres(BlackboxDatabase):
             PGUSER=self.config["username"],
             PGPASSWORD=self.config["password"],
             PGHOST=self.config["host"],
-            PGPORT=self.config["port"],
+            PGPORT=str(self.config.get("port", "5432")),
         )
         log.debug(self.output)
 
