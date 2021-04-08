654299dd9552c9bcb5984998311f7f95aa252de6
Denis Matakov <denis@matakov.com>
2021-04-08 08:56:14 +0300

    Docstrings, emoji constants and replace list comprehension with cycle.
    For better readability use only 2 line max list comprehensions.

--- blackbox/handlers/notifiers/telegram.py
+++ blackbox/handlers/notifiers/telegram.py
@@ -4,8 +4,10 @@ from telebot.apihelper import ApiTelegramException
 from blackbox.handlers.notifiers._base import BlackboxNotifier
 from blackbox.utils.logger import log
 
-
 STRING_LIMIT = 2000  # Limit output
+CHECKMARK_EMOJI = "\U00002705"  # ✔
+FAIL_EMOJI = "\U0000274C"  # ❌
+WARNING_EMOJI = "\U000026A0"  # ⚠
 
 
 class Telegram(BlackboxNotifier):
@@ -19,22 +21,19 @@ class Telegram(BlackboxNotifier):
         for db in self.report.databases:
             data_report += f"{db.database_id}: \n"
             if db.success:
-                # Check storages one by one
-                data_report += "".join(
-                    [
-                        f"\U00002705 {storage.storage_id}\n"
-                        if storage.success is True
-                        else
-                        f"\U0000274C {storage.storage_id}\n"
-                        for storage in db.storages
-                    ])
+                # Check storages one by one.
+                for storage in db.storages:
+                    if storage.success:
+                        data_report += f"{CHECKMARK_EMOJI} {storage.storage_id}\n"
+                    else:
+                        data_report += f"{FAIL_EMOJI} {storage.storage_id}\n"
             else:
-                data_report += "\U0000274C Backup failed\n"
-                data_report += f"\U000026A0 {db.output[:STRING_LIMIT]}\n"
+                data_report += f"{FAIL_EMOJI} Backup failed.\n"
+                data_report += f"{WARNING_EMOJI} {db.output[:STRING_LIMIT]}\n"
         return data_report
 
     def notify(self):
-        """ Convert report dict to string and send via Telegram """
+        """Convert report dict to string and send via Telegram."""
         bot = telebot.TeleBot(self.config["token"])
         try:
             bot.send_message(
@@ -44,5 +43,5 @@ class Telegram(BlackboxNotifier):
         except ApiTelegramException:
             log.debug(
                 "Telegram API key or user_id is wrong "
-                "or you forgot to press /start in your bot"
+                "or you forgot to press /start in your bot."
             )
