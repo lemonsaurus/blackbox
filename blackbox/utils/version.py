2a03b59297b3aee48586779389320a29f1ec4e08
Denis Matakov <denis@matakov.com>
2021-04-11 13:28:59 +0300

    Get semver from Poetry TOML and use it to push to Dockerhub

--- blackbox/utils/version.py
+++ blackbox/utils/version.py
@@ -7,3 +7,8 @@ def get_project_version():
         file_contents = pyproject.read()
 
     return tomlkit.parse(file_contents)['tool']['poetry']['version']
+
+
+# If we call this file we return version for docker push action.
+if __name__ == "__main__":
+    print(get_project_version())
